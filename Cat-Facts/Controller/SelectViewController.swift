9c4b9fddd5a88ff70ec7ff01c28bde17743dc042
bahittinn <bahittin84@gmail.com>
2023-08-05 01:23:40 +0300

    trials

--- Cat-Facts/Controller/SelectViewController.swift
+++ Cat-Facts/Controller/SelectViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class SelectViewController: UIViewController {
 
+    @IBOutlet var Views: [UIView]!
     @IBOutlet weak var catView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         catView.layer.cornerRadius = 8
+        for view in Views {
+            view.layer.cornerRadius = 8
+        }
         // Do any additional setup after loading the view.
     }
     
